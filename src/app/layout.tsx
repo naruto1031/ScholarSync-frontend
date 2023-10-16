89a0422059c860fea9fc5f3cad6e4c639931dd52
naruto1031 <naruto0614@au.com>
2023-10-16 23:15:24 +0900

    不要なimportの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import NextAuthProvider from './components/provider/NextAuthProvider'
 import './globals.css'
-import "../../styled-system/styles.css"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='ja'>
