96cd0809b4a21d5f93d081ed18413456b92bc92f
naruto1031 <naruto0614@au.com>
2024-01-05 00:26:38 +0900

    bodyタグをmargin0に

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import { NextAuthProvider } from '@/app/components'
-import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='ja'>
-			<body>
+			<body
+				style={{
+					margin: 0,
+				}}
+			>
 				<NextAuthProvider>{children}</NextAuthProvider>
 			</body>
 		</html>
