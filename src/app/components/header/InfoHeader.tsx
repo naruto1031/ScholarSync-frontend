68f835d40345abdb9c22659dcdfebc10e3871182
naruto1031 <naruto0614@au.com>
2024-02-06 23:37:58 +0900

    feat: トップページ修正

--- src/app/components/header/InfoHeader.tsx
+++ src/app/components/header/InfoHeader.tsx
@@ -1,13 +1,9 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import { useState } from 'react'
-import { signOut } from 'next-auth/react'
 import Box from '@mui/material/Box'
 import Image from 'next/image'
+import NextLink from 'next/link'
 
 export const InfoHeader = () => {
 	return (
@@ -18,28 +14,36 @@ export const InfoHeader = () => {
 			}}
 		>
 			<Toolbar>
-				<IconButton
-					size='large'
-					edge='start'
-					color='inherit'
-					aria-label='menu'
-					sx={{
-						mr: 2,
-						display: { sm: 'none' },
-					}}
-				>
-					<MenuIcon />
-				</IconButton>
 				<Typography variant='h6' component='div' sx={{ flexGrow: 1 }} color={'black'}>
 					<Box
 						sx={{
 							display: 'flex',
 							alignItems: 'center',
+							'@media (max-width: 800px)': {
+								justifyContent: 'center',
+							},
 						}}
 					>
 						<Image src='/scholar_sync.jpg' width={300} height={47} alt='Picture of the author' />
 					</Box>
 				</Typography>
+				<NextLink
+					href={'./login'}
+					passHref
+					style={{
+						textDecoration: 'none',
+					}}
+				>
+					<Box
+						sx={{
+							textDecoration: 'none',
+							color: '#085FCE',
+							border: 'none',
+						}}
+					>
+						Login
+					</Box>
+				</NextLink>
 			</Toolbar>
 		</AppBar>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Container, Typography, Paper, Grid } from '@mui/material'
+import { Box, Button, Container, Typography, Paper, Grid, Stack } from '@mui/material'
 import NextLink from 'next/link'
 import { Metadata } from 'next'
-import { InfoHeader } from './components'
+import { Footer, InfoHeader } from './components'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
 	title: 'Scholar Sync',
@@ -11,32 +12,255 @@ export const metadata: Metadata = {
 
 const Page = () => {
 	return (
-		<Box>
+		<Box
+			sx={{
+				overflowX: 'hidden',
+			}}
+		>
 			<InfoHeader />
-			<Container maxWidth='lg'>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<Paper elevation={3} sx={{ padding: '30px', textAlign: 'center', marginTop: '20px' }}>
-							<Typography variant='h4' component='h1' gutterBottom sx={{ fontWeight: 'bold' }}>
-								Scholar Syncへようこそ
-							</Typography>
-							<Typography variant='body1' sx={{ marginBottom: '20px' }}>
-								教育の未来を、テクノロジーと共に。Scholar
-								Syncは、教育機関が直面する現代の課題に対応するために設計された、革新的な管理ツールです。Microsoftアカウントとの連携により、安全かつ効率的な学生情報管理を可能にします。信頼性の高いセキュリティ機能で、学生情報を守ります。
-							</Typography>
-							<NextLink href={'./login'} passHref>
-								<Button
-									variant='contained'
-									color='primary'
-									sx={{ borderRadius: '20px', padding: '10px 20px' }}
+			<Box
+				sx={{
+					backgroundColor: '#E2F2FB',
+					py: '30px',
+				}}
+			>
+				<Box
+					sx={{
+						backgroundColor: '#fff',
+						height: '350px',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+						gap: '10px',
+						px: '20px',
+						'@media (max-width: 800px)': {
+							pt: '20px',
+							flexDirection: 'column',
+							height: 'auto',
+						},
+					}}
+				>
+					<Box
+						sx={{
+							'@media (max-width: 800px)': {
+								textAlign: 'center',
+							},
+						}}
+					>
+						<Box>学生情報管理アプリ ScholarSync</Box>
+						<Box
+							sx={{
+								fontSize: '45px',
+								my: '15px',
+							}}
+						>
+							面倒な
+							<span
+								style={{
+									color: '#085FCE',
+								}}
+							>
+								申請手続き
+							</span>
+							をより
+							<span
+								style={{
+									color: '#085FCE',
+								}}
+							>
+								効率的
+							</span>
+							に
+						</Box>
+						<Box>安全に簡単に学生情報を管理できます！！</Box>
+						<NextLink href={'./login'} passHref>
+							<Button
+								variant='contained'
+								color='primary'
+								sx={{ borderRadius: '10px', padding: '10px 20px', mt: '20px', fontWeight: 'bold' }}
+							>
+								今すぐはじめる
+							</Button>
+						</NextLink>
+					</Box>
+					<Box>
+						<Image src='/student.jpg' width={500} height={293} alt='Picture of the author' />
+					</Box>
+				</Box>
+				<Container maxWidth='lg'>
+					<Box
+						sx={{
+							width: '100%',
+							margin: '0 auto',
+						}}
+					>
+						<Box
+							sx={{
+								textAlign: 'center',
+								fontSize: '30px',
+								my: '30px',
+								fontWeight: 'bold',
+							}}
+						>
+							ScholarSyncとは
+						</Box>
+						<Box
+							sx={{
+								fontSize: '20px',
+								textAlign: 'center',
+								my: '20px',
+							}}
+						>
+							学生、教員間の手続きを効率化するための学生情報管理アプリケーションです。
+						</Box>
+						<Box
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								gap: '100px',
+								my: '40px',
+								'@media (max-width: 800px)': {
+									flexDirection: 'column',
+									gap: '20px',
+									justifyContent: 'center',
+								},
+							}}
+						>
+							<Box
+								sx={{
+									width: '300px',
+									'@media (max-width: 800px)': {
+										margin: '0 auto',
+									},
+								}}
+							>
+								<Box
+									sx={{
+										width: 'fit-content',
+										margin: '0 auto',
+									}}
 								>
-									はじめる
-								</Button>
-							</NextLink>
-						</Paper>
-					</Grid>
-				</Grid>
-			</Container>
+									<Image
+										src='/rejoice_man_mono.png'
+										width={200}
+										height={200}
+										alt='Picture of the author'
+									/>
+								</Box>
+								<Box
+									sx={{
+										backgroundColor: '#fff',
+										padding: '30px 20px',
+										borderRadius: '10px',
+										mt: '20px',
+										height: '80px',
+										position: 'relative',
+									}}
+								>
+									<Box
+										sx={{
+											position: 'absolute',
+											top: '-14px',
+											left: '50%',
+											transform: 'translateX(-50%)',
+											fontSize: '20px',
+											backgroundColor: '#E2F2FB',
+											borderRadius: '10px',
+											px: '20px',
+											pb: '5px',
+										}}
+									>
+										学生
+									</Box>
+									面倒な申請手続きを
+									<span
+										style={{
+											color: '#085FCE',
+										}}
+									>
+										ボタン一つ
+									</span>
+									で完了することができ、申請状況を
+									<span
+										style={{
+											color: '#085FCE',
+										}}
+									>
+										簡単に確認
+									</span>
+									することができます。
+								</Box>
+							</Box>
+							<Box
+								sx={{
+									width: '300px',
+									'@media (max-width: 800px)': {
+										margin: '0 auto',
+									},
+								}}
+							>
+								<Box
+									sx={{
+										width: 'fit-content',
+										margin: '0 auto',
+									}}
+								>
+									<Image
+										src='/engineer_suit_mono.png'
+										width={200}
+										height={200}
+										alt='Picture of the author'
+									/>
+								</Box>
+								<Box
+									sx={{
+										position: 'relative',
+										backgroundColor: '#fff',
+										padding: '30px 20px',
+										borderRadius: '10px',
+										mt: '20px',
+										height: '80px',
+									}}
+								>
+									<Box
+										sx={{
+											position: 'absolute',
+											top: '-14px',
+											left: '50%',
+											transform: 'translateX(-50%)',
+											fontSize: '20px',
+											backgroundColor: '#E2F2FB',
+											borderRadius: '10px',
+											px: '20px',
+											pb: '5px',
+										}}
+									>
+										教員
+									</Box>
+									課題表紙の
+									<span
+										style={{
+											color: '#085FCE',
+										}}
+									>
+										一括承認・差戻し
+									</span>
+									、生徒の課題提出状況の一括確認など、
+									<span
+										style={{
+											color: '#085FCE',
+										}}
+									>
+										業務を効率化
+									</span>
+									する機能を提供しています。
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+				</Container>
+			</Box>
+			<Footer />
 		</Box>
 	)
 }
