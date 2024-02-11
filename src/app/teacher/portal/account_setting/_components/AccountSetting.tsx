fd16f193e12a55a7aeb4e59358c2778a53e481fe
naruto1031 <naruto0614@au.com>
2024-02-11 23:44:47 +0900

    feat: アカウント情報一覧画面の実装

--- src/app/teacher/portal/account_setting/_components/AccountSetting.tsx
+++ src/app/teacher/portal/account_setting/_components/AccountSetting.tsx
@@ -1,5 +1,22 @@
+import { getServerSession } from 'next-auth'
 import { AccountSettingContents } from './AccountSettingContents'
+import { options } from '@/app/options'
+import { redirect } from 'next/navigation'
+import { Teacher } from '@/types/api-response-types'
 
 export default async function AccountSetting() {
-	return <AccountSettingContents />
+	const session = await getServerSession(options)
+	if (!session?.user) {
+		redirect('/login')
+	}
+	const teacherInfoResponse = await fetch(`${process.env.API_URL}/api/teacher/info`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${session?.user.accessToken}`,
+		},
+	})
+
+	const teacherInfoData: Teacher[] = await teacherInfoResponse.json()
+	return <AccountSettingContents teacher={teacherInfoData[0]} />
 }
--- src/app/teacher/portal/account_setting/_components/AccountSettingContents.tsx
+++ src/app/teacher/portal/account_setting/_components/AccountSettingContents.tsx
@@ -1,7 +1,73 @@
+'use client'
+import { Teacher } from '@/types/api-response-types'
+import { Button } from '@mui/material'
 import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
 
-// TODO: 承認待ちの課題表紙総数を表示(要対応な情報を表示)
-
-export const AccountSettingContents = () => {
-	return <Box>aaa</Box>
+interface Props {
+	teacher: Teacher
+}
+export const AccountSettingContents = ({ teacher }: Props) => {
+	return (
+		<Box>
+			<Box
+				sx={{
+					my: '20px',
+					fontSize: '20px',
+					fontWeight: 'bold',
+				}}
+			>
+				教師情報
+			</Box>
+			<Paper
+				sx={{
+					p: '20px',
+				}}
+			>
+				<Box
+					sx={{
+						mb: '20px',
+						width: '100%',
+						borderBottom: '1px solid #E0E0E0',
+						paddingBottom: '10px',
+					}}
+				>
+					<Box>氏名</Box>
+					<Box>{teacher.name}</Box>
+				</Box>
+				<Box
+					sx={{
+						mb: '20px',
+						width: '100%',
+						borderBottom: '1px solid #E0E0E0',
+						paddingBottom: '10px',
+					}}
+				>
+					<Box>担当教科</Box>
+					{teacher.teacher_subjects.map((subject) => (
+						<Box key={subject.subject_id}>
+							<Box>{subject.subject_name}</Box>
+						</Box>
+					))}
+				</Box>
+				<Box>
+					<Box>担任クラス</Box>
+					{teacher.class_teacher.map((classTeacher) => (
+						<Box key={classTeacher.class_id}>
+							<Box>{classTeacher.name}</Box>
+						</Box>
+					))}
+				</Box>
+			</Paper>
+			<Box
+				sx={{
+					mt: '20px',
+				}}
+			>
+				<Button variant='contained' color='error'>
+					サインアウト
+				</Button>
+			</Box>
+		</Box>
+	)
 }
